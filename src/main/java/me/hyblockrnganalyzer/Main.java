7da9a60d2e5d10d1e2e182122e8dc4b1ef6a471f
John Doe <doej1367@gmail.com>
2022-01-25 23:17:56 +0100

    updated version number

--- src/main/java/me/hyblockrnganalyzer/Main.java
+++ src/main/java/me/hyblockrnganalyzer/Main.java
@@ -25,7 +25,7 @@ import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
 @Mod(modid = Main.MODID, version = Main.VERSION)
 public class Main {
 	public static final String MODID = "hyblockrnganalyzer";
-	public static final String VERSION = "1.2";
+	public static final String VERSION = "1.3";
 
 	private File logFolder;
 	public String[] logFileNames = { "databaseTreasureChest.txt", "databaseLootChest.txt", "databaseNucleusLoot.txt",
