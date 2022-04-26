c689665ba100a5fcddfbabfd1cfcd43d599e0915
John Doe <doej1367@gmail.com>
2022-04-27 00:32:34 +0200

    updated version number

--- src/main/java/me/hyblockrnganalyzer/Main.java
+++ src/main/java/me/hyblockrnganalyzer/Main.java
@@ -30,7 +30,7 @@ import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
 @Mod(modid = Main.MODID, version = Main.VERSION)
 public class Main {
 	public static final String MODID = "hyblockrnganalyzer";
-	public static final String VERSION = "1.7.3";
+	public static final String VERSION = "1.7.4";
 
 	private TxtDatabase txtDatabase;
 	private Settings settings;
